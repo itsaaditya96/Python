44a03c4a9d1253f61c1a1e5ec2cc2a47b69711cf
Aaditya <DrakeAaditya@users.noreply.github.com>
2018-02-04 20:00:04 +0530

    Create Assignment35.py

---
+++ Assignment 31-40/Assignment35.py
@@ -0,0 +1,9 @@
+java_course = {"John", "Jack", "Jill", "Joe"}
+python_course = {"Jake", "John", "Eric", "Jill"}
+
+print("1.",len(python_course))
+print("2.",java_course)
+print("3.",python_course - java_course)
+print("4.", len(java_course & python_course),java_course & python_course)
+print("5.", len(java_course ^ python_course),java_course ^ python_course)
+print("6.", len(java_course | python_course),java_course | python_course)
